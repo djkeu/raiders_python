6241773969332abe999312d594900e8610632b24
djkeu <mrkooij@gmail.com>
2023-09-04 10:21:27 +0200

    Add background color

--- raiders.py
+++ raiders.py
@@ -14,12 +14,16 @@ class Raiders:
         self.screen = pygame.display.set_mode((960, 640))
         pygame.display.set_caption("Raiders of the Lost Gloves")
 
+        self.bg_color = (230, 230, 130)
+
     def run_game(self):
         """Main loop for the game."""
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                     sys.exit()
+
+            self.screen.fill(self.bg_color)
 
             pygame.display.flip()
 
