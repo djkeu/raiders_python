9aa7057a50ab766c6c883c6ff25d9085c84f167f
djkeu <mrkooij@gmail.com>
2023-09-04 10:10:44 +0200

    Initialize game

--- raiders.py
+++ raiders.py
@@ -4,3 +4,14 @@ print("\n\tRaiders of the Lost Gloves\n")
 import sys
 import pygame
 
+class Raiders:
+    """Class to manage the game."""
+
+    def __init__(self) -> None:
+        """Initialize the game and resources."""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((960, 640))
+        pygame.display.set_caption("Raiders of the Lost Gloves")
+
+    
