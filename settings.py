d475823ab35ed40942eb277582d82e7a8fa15edb
djkeu <mrkooij@gmail.com>
2023-09-04 21:15:16 +0200

    Correct spelling error

--- settings.py
+++ settings.py
@@ -2,8 +2,8 @@ class Settings:
     """Settings for Raiders of the Lost Gloves."""
 
     def __init__(self) -> None:
-        """Initialze game settings."""
-        # Screen
+        """Initialize game settings."""
+        # Screen settings
         self.screen_width = 960
         self.screen_height = 640
         self.bg_color = (230, 230, 130)
