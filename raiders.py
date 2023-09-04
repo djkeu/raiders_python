debddb6345328a641f7d0a8907b8d2b18216fb04
djkeu <mrkooij@gmail.com>
2023-09-04 10:30:33 +0200

    Remove obsolete code

--- raiders.py
+++ raiders.py
@@ -4,17 +4,19 @@ print("\n\tRaiders of the Lost Gloves\n")
 import sys
 import pygame
 
+from settings import Settings
+
 class Raiders:
     """Class to manage the game."""
 
     def __init__(self) -> None:
         """Initialize the game and resources."""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((960, 640))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Raiders of the Lost Gloves")
-
-        self.bg_color = (230, 230, 130)
 
     def run_game(self):
         """Main loop for the game."""
@@ -23,9 +25,10 @@ class Raiders:
                 if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                     sys.exit()
 
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             pygame.display.flip()
+
 
 if __name__ == '__main__':
     raiders = Raiders()
