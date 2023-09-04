69d99ddb72f9a5912ec9a8aecd3f4f1316efc989
djkeu <mrkooij@gmail.com>
2023-09-04 10:26:41 +0200

    Create settings.py, Settings()

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """Settings for Raiders of the Lost Gloves."""
+
+    def __init__(self) -> None:
+        """Initialze game settings."""
+        # Screen
+        self.screen_width = 960
+        self.screen_height = 640
+        self.bg_color = (230, 230, 130)
