afebc0be86e63db62b5840b2aaf0876f887500f3
djkeu <mrkooij@gmail.com>
2023-09-05 20:15:49 +0200

    Update up to 12.2 Game character

--- index.py
+++ index.py
@@ -2,7 +2,11 @@
 
 """ 
 12.1 Blue sky, p.238
-    raiders_python/raiders.py'
-    raiders_python/settings.py
-12.2     
+    raiders.py'
+    settings.py
+12.2 Game character, p.238
+    camera.py
+    images/shutterClosedSmall.png
+    images/shutterOpenedSmall.png
+
 """
