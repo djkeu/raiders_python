27829eaf3d481006ae97c7237dfbdca6b2976a24
djkeu <mrkooij@gmail.com>
2023-09-05 20:29:05 +0200

    Add TODO's

--- raiders.py
+++ raiders.py
@@ -23,11 +23,24 @@ class Raiders:
 
     def run_game(self):
         """Main loop for the game."""
+
         while True:
+
+            # TODO: Move events-code to check_events(self)
             for event in pygame.event.get():
+                # Quit game
                 if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                     sys.exit()
+                
+                # Move the camera to the right
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+                    self.camera.rect.x += 5
+                
+                # Move the camera to the left
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
+                    self.camera.rect.x -= 5
 
+            # TODO: Move code to update_screen(self)
             self.screen.fill(self.settings.bg_color)
             self.camera.blitme()
 
