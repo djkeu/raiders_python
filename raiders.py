63dcc7f850dcd338bc09db87c0dc5e84b53390f2
djkeu <mrkooij@gmail.com>
2023-09-06 13:42:04 +0200

    Set screen to fullscreen

--- raiders.py
+++ raiders.py
@@ -14,8 +14,14 @@ class Raiders:
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+        # Windowed mode
+        # self.screen = pygame.display.set_mode(
+        #    (self.settings.screen_width, self.settings.screen_height))
+        
+        # Full screen
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Raiders of the Lost Gloves")
 
         self.camera = Camera(self)
